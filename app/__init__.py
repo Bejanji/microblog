1c93aad9580a7ec5658bbbbcd587dbcfb17ad22e
Garuuda <60025072+Garuuda@users.noreply.github.com>
2020-08-28 19:03:22 -0400

    first commit

---
+++ app/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from config import Config
+#database
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+#login
+from flask_login import LoginManager
+
+#app and config
+app = Flask(__name__)
+app.config.from_object(Config)
+
+#database
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+#login
+login = LoginManager(app)
+login.login_view = 'login'
+
+from app import routes, models
